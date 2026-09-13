a13b5f72f54b25c2fb61e75d4b5f6753c249eafd
Raghav Goel <raghavgd2h@gmail.com>
2026-09-13 09:48:18 -0500

    add hover name change, triangle to show accordionness, some UI fixes

--- js/index.js
+++ js/index.js
@@ -1,19 +1,11 @@
-// Get the button:
-let mybutton = document.getElementById("back-to-top");
+const nameHeading = document.getElementById("name-heading");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+nameHeading.addEventListener("mouseover", (event) => {
+  nameHeading.innerHTML = "राघव गोयल"
+});
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+nameHeading.addEventListener("mouseout", (event) => {
+  nameHeading.innerHTML = "Raghav Goel"
+});
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+
